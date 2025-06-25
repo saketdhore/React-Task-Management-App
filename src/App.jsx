75b50234ca4e8359f5b71f946a6ea1308116c530
saketdhore <absaket.sd@gmail.com>
2025-06-25 13:27:43 -0500

    Starter components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import CreateProject from "./components/CreateProject";
+import EmptyProject from "./components/EmptyProject";
+import { savedProject } from "./savedProject";
+import { useState } from "react";
 function App() {
+  const [projects, setProjects] = useState(savedProject);
+  const [isCreating, setIsCreating] = useState(false);
+  const handleIsCreate = () =>{
+    setIsCreating(true);
+  }
+  const handleCreateProject = ({title, description, dueDate, tasks=[]}) => {
+    const newProject = {
+      title: title,
+      description: description,
+      dueDate: new Date(dueDate),
+      tasks: tasks
+    };
+
+    setProjects((prevProjects)=>[...prevProjects, newProject]);
+  }
   return (
     <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+      {projects.length ===0 && <EmptyProject onStartCreate={handleIsCreate}/>}
+      {isCreating && <CreateProject/>}
     </>
   );
 }
---
+++ src/components/CreateProject.jsx
@@ -0,0 +1,16 @@
+const CreateProject = () => {
+    return(
+        <form>
+            <button>Cancel</button>
+            <button>Save</button>
+            <p>Title</p>
+            <input type="text"></input>
+            <p>Description</p>
+            <textarea></textarea>
+            <p>Due Date</p>
+            <input type="date"></input>
+        </form>
+    );
+}
+
+export default CreateProject;
